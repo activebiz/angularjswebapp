62bdc073d65f2028bdb31e081a242edda41b3629
Preyash Desai <psdesai@activebiz.co.uk>
2015-04-28 12:45:26 +0000

    add new employee functionality

--- src/app/employees/controllers/EmployeesCtrl.js
+++ src/app/employees/controllers/EmployeesCtrl.js
@@ -5,6 +5,13 @@ define(['employees/module'], function (module) {
     module.registerController('EmployeesCtrl', function ($scope) {
         $scope.title = 'Employee List';
         $scope.employees = [{ name : 'Preyash Desai', age : '22' }, {name : 'Hima Desai', age : '21'}];
+        $scope.add = function(){
+            if($scope.newEmployee !== undefined)
+            {
+                $scope.employees.push($scope.newEmployee);
+                $scope.newEmployee = undefined;
+            }
+        };
     });
 
 });
