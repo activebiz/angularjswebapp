4f08981d0803bc00003ed14182446f5127690285
Preyash Desai <psdesai@activebiz.co.uk>
2015-04-28 12:06:17 +0000

    tidyup

--- src/app/contacts/controllers/ContactCtrl.js
+++
@@ -1,7 +0,0 @@
-define(['app/contacts/module'],
-    function (module) {
-    'use strict';
-    module.registerController('ContactCtrl', function ($scope, $interval) {
-        $scope.message = "hello"
-    });
-});
