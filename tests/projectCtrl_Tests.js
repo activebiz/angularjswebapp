618159d7b7b91e14025c26374b8ef811500e974c
Preyash Desai <psdesai@activebiz.co.uk>
2015-06-17 09:26:13 +0000

    karma unit test package added

---
+++ tests/projectCtrl_Tests.js
@@ -0,0 +1,11 @@
+describe('ProjectCtrl Tests ', function() {
+
+console.log('Loading a web page');
+    // necessary
+    it('should add two numbers correctly', function() {});
+    it('should subtract two numbers correctly', function() {});
+
+    // helpful but not needed
+    it('should add negative numbers', function() {});
+    it('should reject non numbers', function() {});
+});
